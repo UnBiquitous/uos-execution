facff728730cb31795dd3011349d0a7a614b0f2f
Fabricio Nogueira Buzeto <nuk.anime13@gmail.com>
2012-04-19 19:58:37 -0300

    We're already unserializing the agents and running them. But it still lacks of control over execution threads and communication interface.

--- src/test/java/org/unbiquitous/driver/spike/ReflectionSpike.java
+++ src/test/java/org/unbiquitous/driver/spike/ReflectionSpike.java
@@ -3,16 +3,16 @@ package org.unbiquitous.driver.spike;
 import static org.junit.Assert.*;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.lang.reflect.Method;
+import java.io.OutputStream;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
 
 import org.abstractmeta.toolbox.compilation.compiler.JavaSourceCompiler;
 import org.abstractmeta.toolbox.compilation.compiler.impl.JavaSourceCompilerImpl;
 import org.junit.Test;
-import org.junit.rules.TemporaryFolder;
 
 public class ReflectionSpike {
 
@@ -29,28 +29,50 @@ public class ReflectionSpike {
 	      "    }";
 	    compilationUnit.addJavaSource("org.unbiquitous.driver.spike.Foo", javaSourceCode);
 	    ClassLoader classLoader = javaSourceCompiler.compile(compilationUnit);
-	    Class fooClass = classLoader.loadClass("org.unbiquitous.driver.spike.Foo");
+	    @SuppressWarnings("rawtypes")
+		Class fooClass = classLoader.loadClass("org.unbiquitous.driver.spike.Foo");
 	    assertEquals("Hello World 10",((I)fooClass.newInstance()).m(10));
 	}
 	
 	
 	@Test public void serializeDeserializeAndRun() throws Exception{
+		InputStream in = new PipedInputStream();
+		OutputStream out = new PipedOutputStream((PipedInputStream) in);
+
 		I original = new nI();
-		TemporaryFolder tmpFolder = new TemporaryFolder();
-		File file = tmpFolder.newFile("reflection_test");
-		ObjectOutputStream writer = new ObjectOutputStream(new FileOutputStream(file));
+		assertEquals((Integer)1,Spy.count);
+		assertEquals("0 10",original.m(10));
+		
+		ObjectOutputStream writer = new ObjectOutputStream(out);
 		writer.writeObject(original);
 		writer.close();
-		ObjectInputStream reader = new ObjectInputStream(new FileInputStream(file));
+
+		ObjectInputStream reader = new ObjectInputStream(in);
 		I created = (I) reader.readObject();
+		
 		assertNotSame(original, created);
-		assertEquals("my 10",created.m(10));
-		assertEquals("my 5",created.m(5));
-		file.delete();
+		assertEquals((Integer)1,Spy.count);
+		assertEquals("10 5",created.m(5));
 	}
+}
+
+class A {
+	File f;
+}
+
+class Spy {
+	static Integer count = 0;
 }
 
 class nI implements I{
 	private static final long serialVersionUID = 8095143952426379177L;
-	public String m(Integer i) {	return "my "+i;	}
+	private Integer lastNumber = 0;
+	public nI(){
+		Spy.count++;
+	}
+	public String m(Integer i) {
+		String r = lastNumber +" "+i;
+		lastNumber = i;
+		return r;	
+	}
 }
